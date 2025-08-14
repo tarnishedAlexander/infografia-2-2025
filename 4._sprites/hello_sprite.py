6a294e3f63301dc2e37da9d05b6d0fe2c6d0cc56
Jose Laruta <eduardo.laruta@gmail.com>
2025-08-14 07:41:15 -0400

    sprites base2

--- 4._sprites/hello_sprite.py
+++ 4._sprites/hello_sprite.py
@@ -7,11 +7,28 @@ TITLE = "hola arcade"
 class SpriteView(arcade.View):
     def __init__(self):
         super().__init__()
-        self.background_color = arcade.color.YELLOW
+        self.background_color = arcade.color.BLACK
+        self.sprite = arcade.Sprite("4._sprites/img/mario.png", scale=0.5)
+        self.sprite.center_x = WIDTH // 2
+        self.sprite.center_y = HEIGHT // 2
 
     def on_draw(self):
         self.clear()
-
+        arcade.draw_sprite(self.sprite)
+    
+    def on_key_press(self, symbol, modifiers):
+        if symbol == arcade.key.NUM_1:
+            self.sprite.scale = 1
+        elif symbol == arcade.key.NUM_2:
+            self.sprite.scale = 2
+        elif symbol == arcade.key.RIGHT:
+            self.sprite.center_x += 10
+        elif symbol == arcade.key.LEFT:
+            self.sprite.center_x -= 10
+        elif symbol == arcade.key.R:
+            self.sprite.angle += 5
+        
+            
 
 def main():
     window = arcade.Window(WIDTH, HEIGHT, TITLE)
